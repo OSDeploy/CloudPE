d294f6b20037207b55d19ec5de7e9d0bc5ba2a2c
David Segura <david@segura.org>
2021-03-06 02:43:19 -0600

    Update Menu.ps1

--- Menu.ps1
+++ Menu.ps1
@@ -9,7 +9,6 @@ $WC = New-Object System.Net.WebClient
 #$WC.DownloadString("https://raw.githubusercontent.com/OSDeploy/OSD/21.3.5.2/Public/Disk/Get-LocalPartition.ps1")
 
 #Invoke-Expression $WC.DownloadString("https://raw.githubusercontent.com/OSDeploy/OSD/21.3.5.2/Public/Disk/Get-LocalPartition.ps1")
-Pause
 Invoke-Expression ($WC.DownloadString("https://raw.githubusercontent.com/OSDeploy/OSD/21.3.5.2/Public/Disk/Get-LocalPartition.ps1"))
 Pause
 Break
